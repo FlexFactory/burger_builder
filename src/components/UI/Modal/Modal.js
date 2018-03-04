b8a923f50e2953620d62b2ac60d28a34635fbee1
Andrey <muriy.a@outlook.com>
2018-03-04 18:31:40 +0300

    Refactoring tabs

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,9 +3,9 @@ import React from 'react';
 import classes from './Modal.css';
 
 const modal = (props) => (
-  <div className={classes.Modal}>
-    {props.children}
-  </div>
+    <div className={classes.Modal}>
+        {props.children}
+    </div>
 );
 
 export default modal;
